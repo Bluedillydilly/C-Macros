93182bca0ffec42041c7bb2afd192c8702430dee
Bluedillydilly <dylancuprewich@gmail.com>
2021-10-31 16:26:26 -0400

    naming

--- enchanced-for.c
+++ enchanced-for.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-#define FOREACH( v, a ) \
+#define FOREACH_INT( v, a ) \
 	for( int i = 0, v = a[i]; i < sizeof(a) / sizeof(a[0]); i++, v = a[i] )
 int main( void ) {
 	int a[] = {1, 2, 3, 4, 5};	
 	for( int i = 0, c = a[i]; i < sizeof(a) / sizeof(a[0]); i++, c = a[i] ) {
 		printf("%d\n", c );
 	}
-	FOREACH( bb, a ) {
+	FOREACH_INT( bb, a ) {
 		printf("%d\n", bb );
 	}	
 }
